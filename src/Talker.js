e53776b592392d8f50dc1219138577dd13ae6e71
RVeridium <riitta.icer@gmail.com>
2021-08-29 21:59:26 +0300

    initial failures

---
+++ src/Talker.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+import './Frame.css'; 
+
+
+export default function Talker(props) {
+    let xd = props.desc; 
+
+
+
+    return (
+        <div className='Talker'>
+            <text>{xd}</text>
+        </div>
+
+    )
+
+
+
+
+}
+
