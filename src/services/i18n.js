c003496eb92f0c9040e1fc6fb882c97d37d89c63
Elias Mäkelä <eliasjmakela@outlook.com>
2021-09-02 14:14:47 +0300

    LisÃÃ¤ns redundantit kÃÃnnÃkset suomenkielisille teksteille TÃmÃ¤helpottaa esim. korjausten tekemistÃ¤suomenkieliseen tekstiin ilman ettÃ¤muut kÃÃnnÃkset hajoaa

--- src/services/i18n.js
+++ src/services/i18n.js
@@ -1,12 +1,13 @@
-
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import en from './en.json'
 import se from './se.json'
+import fi from './fi.json'
 
 let resources = { 
 en: { translation: en },
-se: {translation: se}
+se: {translation: se},
+fi: {translation: fi}
 };
 
 i18n
