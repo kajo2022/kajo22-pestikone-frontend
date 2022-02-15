e833b311307726ff6f7d4b8db4513c16e3e6dec7
Ode42 <ode.progr@outlook.com>
2022-02-15 17:21:39 +0200

    Closed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import '.'
-import Frame from './components/Frame.jsx';
-import Styles from './assets/styles/Style';
-import { css } from 'aphrodite';
-
+import ".";
+import Frame from "./components/Frame.jsx";
+import Styles from "./assets/styles/Style";
+import { css } from "aphrodite";
+import Closed from "./components/Closed";
 
 const App = () => {
-
-  
   return (
     <div className={css(Styles.app)}>
-      <Frame/>
+      <Closed />
     </div>
   );
-  
-}
+};
 
 export default App;
---
+++ src/components/Closed.jsx
@@ -0,0 +1,22 @@
+const Closed = () => {
+  return (
+    <div className="closed">
+      <h1>
+        Finnjamboree Kajon pestikone on nyt suljettu. Voit hakea pestiin Kajon
+        verkkosivuilta osoitteesta:
+        <a href="https://kajo2022.fi/pestit/">https://kajo2022.fi/pestit/</a>
+      </h1>
+      <br />
+      <h2>
+        The assignment engine of Finnjamboree Kajo has been closed. You can
+        apply for an camp assignment at Kajo's website:{" "}
+        <a href="https://kajo2022.fi/en/camp-assignments/">
+          https://kajo2022.fi/en/camp-assignments/
+        </a>
+      </h2>
+      <br />
+    </div>
+  );
+};
+
+export default Closed;
